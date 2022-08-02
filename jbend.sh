6aae9941003a643fd48aa8f35c19b8779d8d11b5
kanetempleton <ktempleton96@yahoo.com>
2022-08-02 04:00:29 -0500

    config directory

--- jbend.sh
+++ jbend.sh
@@ -1,15 +1,28 @@
 #! /bin/bash
 
-if [ $# -ne 1 ]; then
+if [ ${#JBEND} -lt 1 ]; then
+echo "please set your path to your JBend scripts folder by running:
+      jbend source <fullpath to this folder>"
+fi
+
+if [ $# -lt 1 ]; then
 echo "usage: jbend <getjar,updatejar>"
 else
 
 case $1 in
+source)
+#echo "$2"
+JBEND="$2"
+echo sourced JBend to "$JBEND"
+;;
 getjar)
-~/me/script/jbend-scripts/pulljar.sh
+$JBEND/pulljar.sh
 ;;
 updatejar)
-pwd
+cd universe/res/lib
+rm JBend.jar
+$JBEND/pulljar.sh
+cd ../../..
 ;;
 *)
 echo "undefined option: $1"
