2725cc813942706b4485ecd952ac622669bb93cf
kanetempleton <ktempleton96@yahoo.com>
2022-08-26 12:14:24 -0500

    appconfig added to jbend command

--- jbend.sh
+++ jbend.sh
@@ -24,6 +24,48 @@ rm JBend.jar
 $JBEND/pulljar.sh
 cd ../../..
 ;;
+initapp)
+cd universe
+mkdir -p config
+cd config
+touch application.conf
+echo "app-id: yourApp
+
+data:
+source 127.0.0.1 4343
+
+login:
+source 127.0.0.1 4344
+
+console:
+listen
+
+proxy:
+listen 80
+map 127.0.0.1 4444
+
+webserver:
+listen 4444
+home res/front/
+
+loginserver:
+listen 4344
+
+dataserver:
+listen 4343
+source 127.0.0.1 dbname
+credentials root admin
+
+EOF" > application.conf
+
+cd ..
+touch routes.cfg
+echo "/login -> /pages/login/login.html" > routes.cfg
+
+echo "Config files created. Please check universe/config/application.conf 
+to change parameters that define your application"
+
+;;
 *)
 echo "undefined option: $1"
 ;;
